708dc67cee087edbe1289e0b3202f31e650abfa5
Beatwarez <beatwarez@github.com>
2026-07-18 22:13:12 +0200

    build 1.0.01: switch to sequential build numbering

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // AETHER UI Logic & C++ Host Communication Bridge
-const BUILD_VERSION = 'build c6bb41d';
+const BUILD_VERSION = 'build 1.0.01';
 
 // 18 Sync divisions strings
 const SYNC_DIVISIONS = [
