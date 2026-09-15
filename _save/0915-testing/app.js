f93fb455fb907ec09d61316ad18cb30b3957c25b
Beatwarez <beatwarez@github.com>
2026-09-15 11:12:51 +0200

    Update backup project in _save/0915-testing

--- _save/0915-testing/app.js
+++ _save/0915-testing/app.js
@@ -1,5 +1,5 @@
 // AETHER UI Logic & C++ Host Communication Bridge
-const BUILD_VERSION = '0.0.20';
+const BUILD_VERSION = '0.0.21';
 
 // 18 Sync divisions strings
 const SYNC_DIVISIONS = [
--- _save/0915-testing/installer_win.iss
+++ _save/0915-testing/installer_win.iss
@@ -2,7 +2,7 @@
 
 [Setup]
 AppName=Aether
-AppVersion=0.0.20
+AppVersion=0.0.21
 AppPublisher=Algebra Within
 DefaultDirName={commoncf}\VST3\Algebra Within\Aether.vst3
 DisableDirPage=yes
